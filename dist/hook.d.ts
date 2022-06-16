d74dc037c4183f92ff74691423211c265a83f5b9
kokojer <gordei80z34@gmail.com>
2022-06-16 14:34:29 +0300

    fix bugs v1.1.2

--- dist/hook.d.ts
+++ dist/hook.d.ts
@@ -1,7 +1,7 @@
 interface TypesHook {
     counter: number;
     visible: boolean;
-    onVisibilityChange: (callback: (isVisible?: boolean) => void) => void;
+    onVisibilityChange: (callback: (isVisible: boolean) => void) => void;
 }
 export declare const useDocumentVisibility: () => TypesHook;
 export {};
--- src/hook.ts
+++ src/hook.ts
@@ -3,14 +3,14 @@ import { useEffect, useState } from "react";
 interface TypesHook {
   counter: number;
   visible: boolean;
-  onVisibilityChange: (callback: (isVisible?: boolean) => void) => void;
+  onVisibilityChange: (callback: (isVisible: boolean) => void) => void;
 }
 
+const callbacks: Array<(isVisible: boolean) => void> = [];
 export const useDocumentVisibility = (): TypesHook => {
   const [counter, setCounter] = useState(0);
   const [visible, setVisible] = useState(true);
-  const callbacks: Array<(isVisible?: boolean) => void> = [];
-  const onVisibilityChange = (callback: () => void) => {
+  const onVisibilityChange = (callback: (isVisible: boolean) => void) => {
     callbacks.push(callback);
   };
   useEffect(() => {
@@ -19,7 +19,7 @@ export const useDocumentVisibility = (): TypesHook => {
         ? setVisible(true)
         : setVisible(false);
       if (document.hidden) {
-        setCounter((counter) => ++counter);
+        setCounter((counter) => counter + 1);
       }
       callbacks.forEach((callback) => {
         callback(document.visibilityState === "visible");
@@ -28,7 +28,6 @@ export const useDocumentVisibility = (): TypesHook => {
     document.addEventListener("visibilitychange", handlerVisibilityChange);
     return () => {
       document.removeEventListener("visibilitychange", handlerVisibilityChange);
-      callbacks.splice(0, callbacks.length);
     };
   }, []);
   return {
